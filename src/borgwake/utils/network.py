ed87acf5342e07a637124c5c349019b79d17801f
carip03 <pasqualecaringellab@gmail.com>
2026-07-09 00:03:43 +0200

    refactor: make _clean_mac public

--- src/borgwake/utils/network.py
+++ src/borgwake/utils/network.py
@@ -20,7 +20,7 @@ def is_valid_ip(ip: str) -> bool:
 def is_valid_mac(mac: str) -> bool:
     """Validates MAC address."""
 
-    if re.match("[0-9a-f]{12}$", _clean_mac(mac)):
+    if re.match("[0-9a-f]{12}$", clean_mac(mac)):
         return True
     return False
 
@@ -28,10 +28,10 @@ def is_valid_mac(mac: str) -> bool:
 def compare_mac(mac1: str, mac2: str) -> bool:
     """Compare MAC address."""
 
-    return _clean_mac(mac1) == _clean_mac(mac2)
+    return clean_mac(mac1) == clean_mac(mac2)
 
 
-def _clean_mac(mac: str) -> str:
+def clean_mac(mac: str) -> str:
     """Clean MAC address.
 
     Normalize MAC address to lowercase, remove leading and trailing spaces and
