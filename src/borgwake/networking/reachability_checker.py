0dad4b66592281dba8af6dd79735a557c2fa3b6a
carip03 <pasqualecaringellab@gmail.com>
2026-07-24 01:35:24 +0200

    WIP: modify exception management in ReachabilityChecker

--- src/borgwake/networking/reachability_checker.py
+++ src/borgwake/networking/reachability_checker.py
@@ -7,6 +7,12 @@ logger = logging.getLogger(__name__)
 
 _PING_NUMBER = 4
 _PING_TIMEOUT = 2
+_POLL_TIMEOUT = 90
+_POLL_INTERVAL = 10
+
+
+class ReachabilityCheckError(Exception):
+    """A system error occurred while checking if a remote host is online."""
 
 
 class ReachabilityChecker:
@@ -16,7 +22,13 @@ class ReachabilityChecker:
         self._host_name = host_name
 
     async def is_online(self) -> bool:
-        """Check if remote host is online using ping command."""
+        """Check if remote host is online using ping command.
+
+        Returns:
+            True if the host is reachable, False if it is not reachable.
+        Raises:
+            ReachabilityCheckError: if a system error occurred while checking if remote host is online.
+        """
 
         logger.info("Checking if remote host %s is online...", self._host_name)
 
@@ -40,6 +52,23 @@ class ReachabilityChecker:
             logger.info("Remote host %s is online", self._host_name)
             return True
 
-        except OSError:
-            logger.error("Failed to execute ping command.", exc_info=True)
-            return False
+        except OSError as e:
+            raise ReachabilityCheckError(
+                "An error occurred while checking if remote host is online."
+            ) from e
+
+    async def wait_until_online(self) -> bool:
+        """Poll the host until it responds or the timeout elapses.
+
+        Returns:
+            True if the host became reachable, False if the timeout was reached.
+        Raises:
+            ReachabilityCheckError: if a system error occurred while checking if remote host is online.
+        """
+
+        deadline = asyncio.get_event_loop().time() + _POLL_TIMEOUT
+        while asyncio.get_event_loop().time() < deadline:
+            if await self.is_online():
+                return True
+            await asyncio.sleep(_POLL_INTERVAL)
+        return False
