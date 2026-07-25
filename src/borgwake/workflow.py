1399a965236f68497a2c753ff320ba68a110ad40
carip03 <pasqualecaringellab@gmail.com>
2026-07-25 18:55:41 +0200

    refactor: add workflow.py

--- src/borgwake/workflow.py
+++ src/borgwake/workflow.py
@@ -1,10 +1,23 @@
 """Provides the orchestrator logic to run the whole workflow."""
 
-from borgwake.backup.abstractions import BackupExecutor, BackupJob, BackupStatus
+import logging
+
+from borgwake.backup.abstractions import (
+    BackupExecutionError,
+    BackupExecutor,
+    BackupJob,
+    BackupStatus,
+)
 from borgwake.backup.backup_orchestrator import cycle_backups
-from borgwake.networking.reachability_checker import ReachabilityChecker
-from borgwake.notifier.notifier import Notifier
+from borgwake.networking.reachability_checker import (
+    ReachabilityChecker,
+    ReachabilityCheckError,
+)
+from borgwake.notifier.notifier import Notifier, NotifierError
+from borgwake.power.abstractions import ShutdownFailure, TurnOnFailure
 from borgwake.power.remote_host_power_controller import RemoteHostPowerController
+
+logger = logging.getLogger(__name__)
 
 
 async def run_workflow(
@@ -14,20 +27,74 @@ async def run_workflow(
     executor: BackupExecutor,
     notifier: Notifier,
 ) -> BackupStatus:
+    """Run the whole Borgwake workflow.
+
+    Raises:
+        ShutdownFailure: if an error has occurred while turning off the remote host.
+        NotifierError: if an error has occurred while notifying.
+    """
+
     status = BackupStatus.SUCCESS
+    was_host_online = True
 
-    was_host_online = await reachability_checker.is_online()
-    if not was_host_online:
-        await power_controller.turn_on()
+    try:
+        was_host_online = await reachability_checker.is_online()
+        if not was_host_online:
+            await power_controller.turn_on()
 
-        if not await reachability_checker.wait_until_online():
-            status = BackupStatus.ERROR
+            if not await reachability_checker.wait_until_online():
+                status = BackupStatus.ERROR
+                return status
 
-    if status != BackupStatus.ERROR:
         status = await cycle_backups(jobs, executor)
+        return status
 
-    if not was_host_online:
-        await power_controller.turn_off()
+    except ReachabilityCheckError as e:
+        logger.error(
+            "An error has occurred while trying to check the status of the remote host. Error: %s",
+            e,
+        )
 
-    await notifier.notify(status)
-    return status
+        status = BackupStatus.ERROR
+        return status
+    except TurnOnFailure as e:
+        logger.error(
+            "An error has occurred while trying to turn on the remote host. Error: %s",
+            e,
+        )
+
+        status = BackupStatus.ERROR
+        return status
+    except BackupExecutionError as e:
+        logger.error(
+            "An error has occurred while trying to execute the backup. Error: %s", e
+        )
+        status = BackupStatus.ERROR
+
+        return status
+
+    finally:
+        shutdown_error: ShutdownFailure | None = None
+        try:
+            if not was_host_online:
+                await power_controller.turn_off()
+        except ShutdownFailure as e:
+            logger.error(
+                "An error has occurred while trying to shutdown the remote host. Error: %s",
+                e,
+            )
+
+            shutdown_error = e
+            if status != BackupStatus.ERROR:
+                status = BackupStatus.WARNING
+
+        try:
+            await notifier.notify(status)
+        except NotifierError as e:
+            logger.error(
+                "An error has occurred while trying to notify the notifier. Error: %s",
+                e,
+            )
+            raise
+        if shutdown_error is not None:
+            raise shutdown_error
